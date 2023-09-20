3075fff52fe7c64165757deca1d4c359d876e634
lilvolcanointhenighttime <eliotalderson031@gmail.com>
2023-09-20 07:59:22 +0300

    3

--- 1/Main.java
+++ 1/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Goodbye wrld!");
     }
+
+    public static void main2(String[] args) {
+        System.out.println("Goodbye wrld!");
+    }
+
+    public static void main3(String[] args) {
+        System.out.println("Goodbye wrld!");
+    }
 }
